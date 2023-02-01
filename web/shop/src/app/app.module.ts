265d852e79a79afe88675c5a80dc13bf2a0841aa
Termih <termih@gmail.com>
2023-02-01 09:47:14 +0100

    HttpClientModule és ReactiveFormsModule hozzáadva

--- web/shop/src/app/app.module.ts
+++ web/shop/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
