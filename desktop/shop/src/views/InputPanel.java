4816e2f4efc52293ae73232f21fb35d6b5e678ec
Sallai András <joska@zold.and>
2023-02-13 12:06:31 +0100

    InputPanel kész

--- desktop/shop/src/views/InputPanel.java
+++ desktop/shop/src/views/InputPanel.java
@@ -1,10 +1,36 @@
 package views;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public class InputPanel extends HBox {
-
+    Label label;
+    TextField field;
     public InputPanel() {
+        this.initComponent();
+        this.addComponent();
     }
+    private void initComponent() {
+        this.label = new Label();
+        this.field = new TextField();
+    }
+    private void addComponent() {
+        this.getChildren().add(this.label);
+        this.getChildren().add(this.field);
+    }
+    public String getText() {
+        return label.getText();
+    }
+    public void setText(String text) {
+        this.label.setText(text);
+    }
+    public String getValue() {
+        return field.getText();
+    }
+    public void setValue(String value) {
+        this.field.setText(value);
+    }
+    
     
 }
