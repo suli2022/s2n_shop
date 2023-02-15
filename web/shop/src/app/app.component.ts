cb41aa564452b739fa0013209c2cba206fca40a5
Sallai András <joska@zold.and>
2023-02-15 11:23:23 +0100

    Kilépés működik

--- web/shop/src/app/app.component.ts
+++ web/shop/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './shared/auth.service';
 
 @Component({
@@ -9,13 +10,16 @@ import { AuthService } from './shared/auth.service';
 export class AppComponent {
   title = 'shop';
 
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router
+    ) {}
   
   logout() {
-    console.log("működik");
     this.auth.logout().subscribe({
       next: (res) => {
         console.log(res);
+        this.router.navigate(['/login']);
       },
       error: (err) => {
         console.log(err);
