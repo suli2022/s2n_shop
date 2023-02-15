be933ac807dc1fdc179f00bb5c4282c22b66dcff
Sallai András <joska@zold.and>
2023-02-15 10:47:17 +0100

    logout működik; login javítva

--- web/shop/src/app/app.component.ts
+++ web/shop/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop';
+
+  constructor(private auth: AuthService) {}
+  
+  logout() {
+    console.log("működik");
+    this.auth.logout().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 }
