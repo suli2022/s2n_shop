482267069cf6e133432ea8e6164547ec30db64ff
Sallai András <joska@zold.and>
2023-02-20 11:47:41 +0100

    HttpClient kész

--- desktop/shop/src/models/api/HttpClient.java
+++ desktop/shop/src/models/api/HttpClient.java
@@ -69,6 +69,69 @@ public class HttpClient {
         String text = convertInputStreamToString(inputStream);
         return text;
     }
+
+
+    public String put(String urlStr, String data, HashMap<String, String> headers) {
+        String text;
+        try {
+            text = tryPut(urlStr, data, headers);
+        } catch (IOException e) {
+            text = "Hiba! A PUT művelet nem sikerült a REST API-n!";
+            System.out.println(text);
+        }
+        return text;
+    }
+    public String tryPut(String urlStr, String data, HashMap<String, String> headers)
+            throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("PUT");
+        
+        for( Map.Entry<String, String> entry : headers.entrySet()) {
+            http.setRequestProperty(entry.getKey(), entry.getValue());
+        }
+        
+        http.setDoOutput(true);
+        byte[] dataArray = data.getBytes();
+        OutputStream outputStream = http.getOutputStream();
+        outputStream.write(dataArray);
+        outputStream.close();
+        
+        this.responseCode = http.getResponseCode();
+        InputStream inputStream = http.getInputStream();
+        String text = convertInputStreamToString(inputStream);    
+        return text;
+    }
+
+    public String delete(String urlStr, HashMap<String, String> headers) {
+        String text;
+        try {
+            text = tryDelete(urlStr, headers);
+        } catch (IOException e) {
+            text = "Hiba! A DELETE művelet nem sikerült a REST API-n";
+            System.err.println(text);
+        }
+        return text;
+    }
+    public String tryDelete(String urlStr, HashMap<String, String> headers)
+            throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("DELETE");
+        
+        for( Map.Entry<String, String> entry : headers.entrySet()) {
+            http.setRequestProperty(entry.getKey(), entry.getValue());
+        }
+        
+        http.setDoOutput(true);
+
+        this.responseCode = http.getResponseCode();
+        InputStream inputStream = http.getInputStream();
+        String text = convertInputStreamToString(inputStream);           
+        return text;
+    }
+
+
     private String convertInputStreamToString(InputStream inputStream) {
         String text;
         try {
