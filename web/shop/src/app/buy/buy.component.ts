78ce66fbc52feba52669dd816ff6ca8316ab7249
Sallai András <joska@zold.and>
2023-03-01 09:55:13 +0100

    A termékek megjelennek a website komponenseben

--- web/shop/src/app/buy/buy.component.ts
+++ web/shop/src/app/buy/buy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-buy',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buy.component.scss']
 })
 export class BuyComponent implements OnInit {
-
-  constructor() { }
+  products !: any;
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
-
+  getProducts() {
+    this.api.getProducts().subscribe({
+      next: (result) => {
+        console.log(result)
+        this.products = result;
+      },
+      error: (err) => {}
+    })
+  }
 }
