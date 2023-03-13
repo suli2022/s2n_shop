f12dadb09d0e90fde61828807da21bf50edbae86
Sallai András <joska@zold.and>
2023-03-13 11:23:50 +0100

    A MainTab-on megjelenik a RegistryView

--- desktop/shop/src/views/MainTab.java
+++ desktop/shop/src/views/MainTab.java
@@ -2,14 +2,17 @@ package views;
 
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import views.registry.RegistryView;
 
 public class MainTab extends TabPane {
     Tab productTab;
     Tab loginTab;
     Tab registryTab;
     MainView mainView;
+    RegistryView registryView;
     public MainTab(MainView mainView ) {
         this.mainView = mainView;
+        this.registryView = new RegistryView();
         this.initComponent();
         this.addComponent();
         
@@ -17,7 +20,7 @@ public class MainTab extends TabPane {
     private void initComponent() {
         this.productTab = new Tab("Termékek", this.mainView);
         this.loginTab = new Tab("Belépés");
-        this.registryTab = new Tab("Regisztráció");
+        this.registryTab = new Tab("Regisztráció", this.registryView);
     }
     private void addComponent() {
         this.getTabs().add(this.productTab);
